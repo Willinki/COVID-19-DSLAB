0091ebe1f3f7aee2a4c3d5f346e37e8175cb378a
Willinki <davide.badalotti@outlook.it>
2020-05-06 15:23:40 +0200

    inizio per un modello inutilmente complicato

--- from_month_to_daily.R
+++ from_month_to_daily.R
@@ -17,7 +17,7 @@ expand <- function(df_g){
   #salvato il territorio e il sesso del dataframe
   terr <- df_g$Territorio[1]
   sex <- df_g$Sesso_c[1]
-  prov <- df_g
+  print(terr)
   #si crea un dataframe con sesso e territorio ripetuti
   #per ogni giorno compreso tra data min e max
   daily_df <- data.frame(Territorio = rep(terr), 
@@ -32,11 +32,28 @@ expand <- function(df_g){
     mutate(Value = as.integer(na.approx(Value_i))) %>% 
     select(-Value_i)
   
-  #vengono esclusi i mesi da maggio in poi
-  daily_data <- daily_data %>% 
-    filter(month(daily_data$DATE) <=  5)
   
   return(daily_data)
+}
+
+forecast_2020 <- function(df_c){
+  if(max(df_c$DATE) != as.Date("2019-11-01")) return(df_c)
+  min_date <- df_c$DATE[1]
+  max_date <- as.Date("2020-04-01", format = "%Y-%m-%d")
+  tsex <- df_c$Sesso_c[1]
+  terr <- df_c$Territorio[1]
+  dates <- seq(min_date, max_date, by="months")
+  print(terr)
+  
+  d_ts <- as.xts(df_c$Value_i, order.by = as.Date(df_c$DATE, format = "%Y-%m-%d"))
+  model1 <- auto.arima(d_ts)
+  df_c_pred <- forecast(model1, h = 5) 
+  Value_extended <- c(df_c$Value_i, as.integer(df_c_pred$mean)) 
+  df_c_extended <- data.frame(Territorio = rep(terr), 
+                              Sesso_c = rep(tsex), 
+                              DATE = dates, 
+                              Value_i = Value_extended)
+  return(df_c_extended)
 }
 
 #il dataset viene manipolato
@@ -45,7 +62,8 @@ t_mf <- read_csv(file ="data/raw/popolazione_raw.csv") %>%
   mutate(DATE = as.Date(paste0(TIME, "-01"), format = "%Y-%m-%d")) %>% 
   mutate(Value_i = as.integer(Value)) %>% 
   mutate(Sesso_c = toupper(Sesso)) %>% 
-  select(-TIME, -Value, -Sesso) 
+  select(-TIME, -Value, -Sesso) %>% 
+  filter(Sesso_c == "TOTALE")
 
 #creo un dataset con le province e uno con i comuni
 p_mf <- t_mf %>% filter(str_detect(ITTER107, "IT"))
@@ -53,23 +71,27 @@ c_mf <- t_mf %>% filter(!str_detect(ITTER107, "IT"))
 
 #viene poi suddiviso, ad ognuno viene applicata la funzione expand, 
 #e in seguito ricombinato
-pr_final <- p_mf %>% select(-ITTER107) %>% 
-  group_by(Territorio, Sesso_c) %>% 
-  group_split() %>% 
-  lapply(expand) %>% 
-  bind_rows() 
+l_final <- p_mf %>% select(-ITTER107) %>% 
+  group_by(DATE, Sesso_c) %>% 
+  summarise(Value_i = sum(Value_i), 
+            Territorio = rep("Lombardia")) %>%
+  as.data.frame() %>%
+  forecast_2020() %>%
+  expand()
 
 cm_final <- c_mf %>% select(-ITTER107) %>% 
   group_by(Territorio, Sesso_c) %>% 
   group_split() %>% 
-  lapply(expand) %>% 
+  lapply(forecast_2020) %>% 
+  lapply(expand) %>%
   bind_rows()
 
 #le colonne vengono infine rinominate per essere uguali 
 #al dataset sui decessi
 names(cm_final) <- c("NOME_COMUNE", "SESSO", "DATA", "POPOLAZIONE")
-names(pr_final) <- c("NOME_PROVINCIA", "SESSO", "DATA", "POPOLAZIONE")
+names(l_final) <- c("REGIONE" , "SESSO", "DATA", "POPOLAZIONE")
 
+plot(l_final$Value)
 #viene salvato come .csv
+write_csv(l_final, path = "data/raw/popolazione_lombardia_giorno.csv")
 write_csv(cm_final, path = "data/raw/popolazione_comuni_giorno.csv")
-write_csv(pr_final, path = "data/raw/popolazione_province_giorno.csv")
